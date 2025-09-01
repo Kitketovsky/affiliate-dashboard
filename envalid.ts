28221739a4e8d7c8186e5423c266f2b30b51802e
Kitketovsky <kitketovsky@gmail.com>
2025-09-01 16:44:53 +0400

    chore(env): packages installed, env validation using envalid library

---
+++ envalid.ts
@@ -0,0 +1,10 @@
+import { cleanEnv, str, url } from 'envalid';
+
+export const env = cleanEnv(process.env, {
+	SUPABASE_DATABASE_URL: url({
+		example: 'postgresql://postgres:postgres@127.0.0.1:54322/postgres'
+	}),
+	NEXT_PUBLIC_SUPABASE_URL: url(),
+	NEXT_PUBLIC_SUPABASE_ANON_KEY: str(),
+	SUPABASE_SERVICE_ROLE_KEY: str()
+});
